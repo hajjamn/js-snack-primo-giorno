d7e063c5562f24f355cc4f8b54dbe0cefc269182
hajjamn <hajjamn@gmail.com>
2024-03-01 13:09:50 +0100

    ADD: assignment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('ciao mondo')
+/* ESERCIZIO 1 */
+
+const number = 100; //number
+
+console.log(number)
+
+/* ESERCIZIO 2 */
+
+const PI = 3.14; //number
+ console.log(PI)
+
+ /* ESERCIZIO 2/B */
+
+/*  PI = 3.1416; //number */
+
+ /* Dovrebbe dare errore perche le const non possono cambiare */
+
+/*  console.log(PI) */
+
+ /* ESERCIZIO 2/C*/
+
+ let radius = 8; //number
+ let circle = radius * 2 * PI; //number
+
+ console.log(circle)
+
+ /* ESERCIZIO 3 */
+
+ let name = 'Carlo'; //string
+ name = 'Marco' //string
+
+ console.log(name)
